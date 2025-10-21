49c76fbaa04144fad3cf69928cbe01f6b22b4282
Isaac <shug7626@OX.AC.UK>
2025-10-21 14:34:24 +0100

    Added a function to help fsolve find a solution for J

--- InitialSimulations/ParasiticResistanceSimulation.m
+++ InitialSimulations/ParasiticResistanceSimulation.m
@@ -4,18 +4,21 @@
 J_sc = 38.1;    % Short circuit current density (=38.1 for poly-Si)(mA/cm2)
 V_oc = 0.654;   % Open circuit voltage (=0.654 for poly_Si)(V)
 A = 1;          % Area (cm2)
+R_s = [0, 1, 2];        % Series resistance (Ohm)
+R_s0 = 0;
+R_sh = [Inf, 1e5, 1e4]; % Parallel (shunt) resistance (Ohm)
+R_sh0 = Inf;
 T = 300;        % Temperature (K)
 n = 100;        % Number of voltages to calculate for
 
 % Constants
 q = 1.602e-19;  % Charge of an electron (C)
 k_B = 1.38e-23; % Boltzmann constant (J/K)
-R_s = [0, 1, 2];        % Series resistance (Ohm)
-R_sh = [Inf, 1e5, 1e4]; % Parallel (shunt) resistance (Ohm)
 
 % Variables
 V = linspace(0, V_oc, n);
-J = zeros(length(R_s), n);
+J_s = zeros(length(R_s), n);
+J_sh = zeros(length(R_sh), n);
 P = zeros(1, n);
 
 
@@ -24,10 +27,8 @@ P = zeros(1, n);
 % Using the expression for J_0 from the ideal case
 J_0 = J_sc * (exp((V_oc * q)/(k_B * T)) - 1)^-1;
 
-% Current density for the ideal case
-J_i = J_sc - J_0*(exp((q * V)/(k_B * T)) - 1);
-
-% Rearanging eq1.11, J = (Ji Rsh - V)/(A Rs + Rsh)
-for i = 1:length(R_s)
-    J(i,:) = ((J_i * R_sh(i)) - V)./((A * R_s(i)) + R_sh(1));
-end
+% Using fsolve to find J in the various series resistor values
+i = 1;
+fun = @(J_s)calculate_Js(J_s, J_sc, J_0, V, A, R_s, R_sh0, q, k_B, T, i);
+J0 = 35;
+J_s = fsolve(fun, J0);
