2d171a88b98e8ea4d2bfdc4099e92534aeb5c384
Isaac <shug7626@OX.AC.UK>
2025-11-04 22:10:21 +0000

    Manually set the short circuit current densities as the calculation was giving values which are significantly too small.

--- BasicTandemSimulations/BasicTandemSim.m
+++ BasicTandemSimulations/BasicTandemSim.m
@@ -6,26 +6,28 @@
 % Cell 2 - PVK
 
 % Parameters
-Rs1 = 1;        % Series resistance (Ohm/cm2)
-Rs2 = 3;
-params.Rsh1 = 10e3;    % Shunt (parallel) resistance (Ohm/cm2)
-params.Rsh2 = 1e3;
-params.J01 = 2e-12;    % A/cm2
-params.J02 = 1e-15;
-params.Voc1 = 0.654;   % Open circuit voltage (V)
-params.Voc2 = 1.075;
+Rs1 = 0;        % Series resistance (Ohm/cm2) (=1)
+Rs2 = 0;        % (=3)
+params.Rsh1 = Inf;    % Shunt (parallel) resistance (Ohm/cm2) (=10e3)
+params.Rsh2 = Inf;     % (=1e3)
+params.J01 = 3.9e-10;    % A/cm2 (=2e-12)
+params.J02 = 2.6e-15;    % (=1e-15)
+params.Voc1 = 0.7;   % Open circuit voltage (V) (=0.654)
+params.Voc2 = 1.085;   % (=1.075)
 params.N1 = 1.2;       % Ideality factor
 params.N2 = 1.4;
 params.A = 1;          % Area (cm2)
 T = 300;        % Temperature (Kelvin)
 n = 100;        % Number of points to calculate
 
+V_calc = params.Voc1 + params.Voc2 + 0.28;
+
 % Constants
 q = 1.602e-19;  % Charge of an electron (C)
 k_B = 1.38e-23; % Boltzmann constant (J/K)
 
 % Variables
-V = linspace(0, params.Voc1 + params.Voc2, n);
+V = linspace(0, V_calc, n);
 V1 = zeros(size(V));
 V2 = zeros(size(V));
 J = zeros(0, n);
@@ -35,9 +37,10 @@ J = zeros(0, n);
 params.Rs = Rs1 + Rs2;
 params.Vt = k_B * T/q;
 
-params.Jsc1 = params.J01 * (exp(params.Voc1/params.Vt) - 1);
-params.Jsc2 = params.J02 * (exp(params.Voc2/params.Vt) - 1);
-
+% params.Jsc1 = params.J01 * (exp(params.Voc1/(params.Vt * params.N1)) - 1);
+% params.Jsc2 = params.J02 * (exp(params.Voc2/(params.Vt * params.N2)) - 1);
+params.Jsc1 = 12.33;
+params.Jsc2 = 13.03;
 
 %% Calculations
 options = optimoptions('fsolve', 'Display', 'none');
