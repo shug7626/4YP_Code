af0e26e09b83c8055abd370a2bcb24a4e193282c
Isaac <shug7626@OX.AC.UK>
2026-02-03 15:43:32 +0000

    Upadated the functions to include the area multiplication of the shunt resisance

--- IdealityFactorFreeTandemSims/evaluate_NFree_tandem.m
+++ IdealityFactorFreeTandemSims/evaluate_NFree_tandem.m
@@ -7,11 +7,13 @@ function F = evaluate_NFree_tandem(x, V, params)
     % Using the equations from Jenny Nelson's 'The Physics of Solar Cells'
     % Cell 1 Voltage
     F(1) = J - (params.Jsc1 - (params.J_diff01 + params.J_rad01)*(exp(V1/params.Vt) - 1) ...
-        - params.J_scr01*(exp(V1/(2 * params.Vt)) - 1) + V1/params.Rsh1);
+        - params.J_scr01*(exp(V1/(2 * params.Vt)) - 1) ...
+        + V1/(params.Rsh1 * params.A));
     
     % Cell 2 Voltage
     F(2) = J - (params.Jsc2 - (params.J_diff02 + params.J_rad02)*(exp(V2/params.Vt) - 1) ...
-        - params.J_scr02*(exp(V2/(2 * params.Vt)) - 1) + V2/params.Rsh2);
+        - params.J_scr02*(exp(V2/(2 * params.Vt)) - 1) ...
+        + V2/(params.Rsh2 * params.A));
     
     % Total Voltage
     F(3) = V - (V1 + V2 - J*params.A*params.Rs);
