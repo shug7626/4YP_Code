67217b1d17670ebf83e727b3267b7939594306c0
Isaac <shug7626@OX.AC.UK>
2026-02-11 19:45:22 +0000

    Added shunt resistance to the current density equation

--- LogBasedSim/Single_Non-Ideal/evaluate_single_non_ideal.m
+++ LogBasedSim/Single_Non-Ideal/evaluate_single_non_ideal.m
@@ -6,7 +6,7 @@ function F = evaluate_single_non_ideal(x, V, params)
     % Current equation
     F(1) = (((V1 - params.Vbi)/params.VT) * log((params.Jdiffd + params.Jradd) ...
         + params.Jscrd * exp(-(V1 - params.Vbi)/(2*params.VT)))) ...
-        - log(params.Jillum - J);
+        - log(params.Jillum - (V1 / (params.A * params.Rsh)) - J);
     
     % Voltage equation
     F(2) = V + (J * params.A * params.Rs) - V1;
