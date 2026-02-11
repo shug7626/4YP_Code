9346ed907b7c8d15f5567317477b5486d0cdbaa9
Isaac <shug7626@OX.AC.UK>
2026-02-11 10:54:10 +0000

    Added the parameters section

--- LogBasedSim/Single_Ideal.m
+++ LogBasedSim/Single_Ideal.m
@@ -1,2 +1,24 @@
 % Code based on the log form of the three diode model
 
+% Units as in the Parameters Table
+% Parameters
+T = 300;
+
+% Cell Properties
+ni = 1e10;
+Nd = 2e17;
+Na = 1e17;
+n = 2e17;
+p = 1e17;
+Dn = 0;
+Dp = 0;
+Ln = 1;
+Lp = 1;
+tn = 1e-3;
+tp = 1e-3;
+beta = 0;
+eps = 1;
+
+% Constants
+q = 1.602e-19;
+k = 1.38e-23;
