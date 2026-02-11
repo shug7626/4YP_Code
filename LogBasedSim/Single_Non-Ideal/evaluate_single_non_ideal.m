4595c3ba20d647d3c5fb1fb38d7bc19a3fc91ba5
Isaac <shug7626@OX.AC.UK>
2026-02-11 18:06:54 +0000

    Added function definition

---
+++ LogBasedSim/Single_Non-Ideal/evaluate_single_non_ideal.m
@@ -0,0 +1,13 @@
+function F = evaluate_single_non_ideal(x, V, params)
+    % Unpack input
+    J = x(1);
+    V1 = x(2);
+
+    % Current equation
+    F(1) = (((V1 - params.Vbi)/params.VT) * log((params.Jdiffd + params.Jradd) ...
+        + params.Jscrd * exp(-(V1 - params.Vbi)/(2*params.VT)))) ...
+        - log(params.Jillum - J);
+    
+    % Voltage equation
+    F(2) = V + (J * params.A * params.Rs) - V1;
+end
