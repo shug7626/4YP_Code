7ac66194971f2a79c0492aec3e4406f898ca5864
Isaac <shug7626@OX.AC.UK>
2026-02-11 17:53:04 +0000

    Added the three diode equation calculations

--- LogBasedSim/Single_Non-Ideal/Single_Non_Ideal.m
+++ LogBasedSim/Single_Non-Ideal/Single_Non_Ideal.m
@@ -28,3 +28,31 @@ eps = 11.7 * 8.854e-14;
 % Constants
 q = 1.602e-19;
 k = 1.38e-23;
+
+
+
+%% Diode Constant Calculations
+% Thermal voltage
+params.VT = k*T/q;
+
+% Built in voltage
+params.Vbi = params.VT * log(Na * Nd / (ni ^ 2));
+
+% Diffusion current constant
+Jdiff0 = q * (ni^2) * ((Dn / (Na * Ln)) + (Dp / (Nd * Lp)));
+params.Jdiffd = q * Nd * Na * ((Dn / (Na * Ln)) + (Dp / (Nd * Lp)));
+
+% Radiative recombination current constant
+Jrad0 = beta * ((n * p) - (ni ^ 2));
+params.Jradd = beta * ((n * p * exp(params.Vbi/params.VT)) - (Nd * Na));
+
+% Recombination in the depletion region (SCR) current constant
+Jscr0 = (ni ^ 2) * sqrt(2 * q * eps * ((1/Na) + (1/Nd)) * params.Vbi / (tn * tp));
+params.Jscrd = Nd * Na * sqrt(2 * q * eps * ((1/Na) + (1/Nd)) * params.Vbi / (tn * tp)) ...
+    * exp(-params.Vbi / (2 * params.VT));
+
+% Illumination current
+params.Jillum = Jsc + Jdiff0 + Jrad0 + Jscr0;
+
+
+
