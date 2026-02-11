6ce6502a848efaab7b51741b21ed013feed5d76a
Isaac <shug7626@OX.AC.UK>
2026-02-11 17:37:44 +0000

    Updated file structure

---
+++ LogBasedSim/Single_Ideal/Single_Ideal.m
@@ -0,0 +1,119 @@
+% Code based on the log form of the three diode model
+
+% Units as in the Parameters Table
+% Calculation parameters
+N = 100;        % Number of points to perform the calculation on
+
+% Parameters
+T = 300;
+
+% Cell Properties
+% Jsc = 38.1;
+% ni = 1.45e10;
+% Nd = 1.7e15;
+% Na = 1e15;
+% n = 1.7e15;
+% p = 1e15;
+% Dn = 0;
+% Dp = 0;
+% tn = 1000e-6;
+% tp = 1000e-6;
+% Ln = 1;
+% Lp = 1;
+% beta = 0;
+% eps = 11.7 * 8.854e-10;
+
+Jsc = 38.1;
+ni = 1.45e10;
+Nd = 5e16;
+Na = 1.5e15;
+n = 1.4e5;
+p = 1.5e15;
+Dn = 38.7;
+Dp = 11.61;
+tn = 1000e-6;
+tp = 1000e-6;
+Ln = sqrt(Dn*tn);
+Lp = sqrt(Dp*tp);
+beta = 0;
+eps = 11.7 * 8.854e-14;
+
+% Constants
+q = 1.602e-19;
+k = 1.38e-23;
+
+
+
+%% Diode Constant Calculations
+% Thermal voltage
+params.VT = k*T/q;
+
+% Built in voltage
+params.Vbi = params.VT * log(Na * Nd / (ni ^ 2));
+
+% Diffusion current constant
+Jdiff0 = q * (ni^2) * ((Dn / (Na * Ln)) + (Dp / (Nd * Lp)));
+params.Jdiffd = q * Nd * Na * ((Dn / (Na * Ln)) + (Dp / (Nd * Lp)));
+
+% Radiative recombination current constant
+Jrad0 = beta * ((n * p) - (ni ^ 2));
+params.Jradd = beta * ((n * p * exp(params.Vbi/params.VT)) - (Nd * Na));
+
+% Recombination in the depletion region (SCR) current constant
+Jscr0 = (ni ^ 2) * sqrt(2 * q * eps * ((1/Na) + (1/Nd)) * params.Vbi / (tn * tp));
+params.Jscrd = Nd * Na * sqrt(2 * q * eps * ((1/Na) + (1/Nd)) * params.Vbi / (tn * tp)) ...
+    * exp(-params.Vbi / (2 * params.VT));
+
+% Illumination current
+params.Jillum = Jsc + Jdiff0 + Jrad0 + Jscr0;
+
+
+
+%% Calculate the open circuit voltage of the cell
+% Set fsolve to not display each calculation
+options = optimoptions('fsolve', 'Display', 'none');
+
+% Set initial guess for Voc
+v0 = 0.5;
+
+% Find where the total current is zero
+func = @(v) evaluate_single_ideal_Voc(v, params);
+Voc = fsolve(func, v0, options);
+
+
+
+%% Set range of voltages and vectors to store results
+V = linspace(0, Voc, N);
+J = zeros(size(V));
+
+
+
+%% Calculate J for each V
+% Set initial guess
+j0 = Jsc / 2;
+
+for iter = 1:N
+   fun = @(j)evaluate_single_ideal(j, V(iter), params);
+   J_sol = fsolve(fun, j0, options);
+   J(iter) = real(J_sol);
+end
+
+
+
+%% Plot
+figure(1);
+plot(V,J);
+xline(0);
+yline(0);
+xlabel('Bias Voltage (V)');
+ylabel('Current Density (mA/cm2)')
+title('Single, Ideal Current Density - Voltage Plot');
+
+figure(2);
+plot(V, J.*V);
+xline(0);
+yline(0);
+xlabel('Bias Voltage (V)');
+ylabel('Power Density (mW/cm2)');
+title('Single, Ideal Power Density - Voltage Plot');
+
