d865e771eefb62e04bb5213e1e3db734b5ef2015
Isaac <shug7626@OX.AC.UK>
2026-02-26 14:17:55 +0000

    Now calculates the short circuit current density for cell 1

--- TandemPVK-Si/PVK_Si_main.m
+++ TandemPVK-Si/PVK_Si_main.m
@@ -9,11 +9,13 @@ set = tandem_settings();
 
 %% Calculate the short circuit current densities based on incident spectrum
 % Import the spectrum data
-spectrum_table = readmatrix("Spectrum.xlsx");
+spectrum_table = readmatrix("Spectrum_full.xlsx");
 wavelengths = spectrum_table(:,1);
-photon_flux = spectrum_table(:,2);
+bs = spectrum_table(:,2);
 
 % Convert the wavelengths to photon energies (converting from nm to m)
 E = params.h * params.c ./(wavelengths / 1e9);
 
 % Evaluate Jsc1 by summing each valid discrete value
+validE1 = E(:) >= params.Eg1;
+Jsc1 = params.q * sum(params.etac1 * (1-params.R1) * params.a1 * bs(validE1));
