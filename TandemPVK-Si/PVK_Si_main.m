ea15cf614a61695cfe26cebc5602943924165962
Isaac <shug7626@OX.AC.UK>
2026-02-26 12:17:57 +0000

    Now imports the settings from the settings file

--- TandemPVK-Si/PVK_Si_main.m
+++ TandemPVK-Si/PVK_Si_main.m
@@ -1,5 +1,6 @@
 % Code based on the log form of the three diode model, with series and
 % shunt resistors for a perovskite on silicon tandem cell
 
-% Fetch parameters
+% Fetch parameters and settings
 params = parameters();
+set = tandem_settings();
