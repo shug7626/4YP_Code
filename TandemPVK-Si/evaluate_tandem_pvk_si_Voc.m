9b47e6041ba79ebcb4fcda2cdbf09d5034d5ead5
Isaac <shug7626@OX.AC.UK>
2026-02-26 11:53:34 +0000

    Copied the function for finding the open circuit voltage of the cells from the log-based solution

---
+++ TandemPVK-Si/evaluate_tandem_pvk_si_Voc.m
@@ -0,0 +1,15 @@
+function F = evaluate_tandem_pvk_si_Voc(v, params)
+    % Unpack inputs
+    V1 = v(1);
+    V2 = v(2);
+    
+    % Cell 1
+    F(1) = ((V1 - params.Vbi1) / params.VT) + log(abs((params.Jdiffd1 + params.Jradd1) ...
+        + (params.Jscrd1 * exp(-(V1 - params.Vbi1)/(2 * params.VT))))) ...
+        - log(abs(params.Jillum1 - (V1 / (params.A * params.Rsh1))));
+    
+    % Cell 2
+    F(2) = ((V2 - params.Vbi2) / params.VT) + log(abs((params.Jdiffd2 + params.Jradd2) ...
+        + (params.Jscrd2 * exp(-(V2 - params.Vbi2)/(2 * params.VT))))) ...
+        - log(abs(params.Jillum2 - (V2 / (params.A * params.Rsh2))));
+end
