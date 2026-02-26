9cac443908d4bc95745242a121197c6cb8bbacb2
Isaac <shug7626@OX.AC.UK>
2026-02-26 12:27:40 +0000

    Now imports the spectrum table and separates the wavelength column from the flux column

--- TandemPVK-Si/PVK_Si_main.m
+++ TandemPVK-Si/PVK_Si_main.m
@@ -1,6 +1,14 @@
 % Code based on the log form of the three diode model, with series and
 % shunt resistors for a perovskite on silicon tandem cell
 
-% Fetch parameters and settings
+%% Fetch parameters and settings
 params = parameters();
 set = tandem_settings();
+
+
+
+%% Calculate the short circuit current densities based on incident spectrum
+% Import the spectrum data
+spectrum_table = readtable("Spectrum.xlsx");
+wavelengths = spectrum_table(:,1);
+photon_flux = spectrum_table(:,2);
