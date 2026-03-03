82bb19102c8099127a5364ab8748a566f86a7ff7
Isaac <shug7626@OX.AC.UK>
2026-03-03 11:06:08 +0000

    Added the PVK parameters

---
+++ SinglePVK/parameters_template.m
@@ -0,0 +1,13 @@
+% File to store and load the parameters of the PVK cell
+
+function p = parameters()
+    % Calculation parameter
+    
+    % Physical parameters
+    
+    % PVK parameters
+    
+    % Constants
+    p.q = 1.602e-19;
+    p.k = 1.38e-23;
+end
