48ac4b72ee6594954c2f0742489079d2a4a928e8
Isaac <shug7626@OX.AC.UK>
2026-03-03 17:30:39 +0000

    Added n^- and p^+ definitions and use them in the recombination rates definitions

--- SinglePVK/calculate_JPSC.m
+++ SinglePVK/calculate_JPSC.m
@@ -6,37 +6,41 @@
 
 function J = calculate_JPSC(par, Vin, options)
     % Set the initial conditions
-    V0 = 0.2;
-    Q0 = 1e-2;
+    V0 = (par.Vbi - Vin)/4;
+    Q0 = evaluate_Q(V0, par);
     x0 = [V0, V0, V0, V0, Q0];
     
     % Solve for the internal voltages V1-4
     Vfunc = @(x) solve_pvk_v(x, Vin, par);
     V = fsolve(Vfunc, x0, options);
 
-    % Calculate n and p doping
+    % Calculate n and p concentrations
     n = par.nb * exp(-(V(1) + V(2))/par.VT);
     p = par.pb * exp(-(V(3) + V(4))/par.VT);
+    
+    % Calculate n- and p+ concentrations
+    nn = par.nb * exp(-(V(1) + V(2) + V(3))/par.VT);
+    pp = par.nb * exp(-(V(2) + V(3) + V(4))/par.VT);
     
     % Calculate the recominbation rates
     R = zeros(1,5);
     R(1) = par.beta * n * p;
     R(2) = p / par.taup;
     R(3) = n / par.taun;
-    R(4) = par.vpE * p;
-    R(5) = par.vnH * n;
+    R(4) = par.vpE * pp;
+    R(5) = par.vnH * nn;
     
-    % Calculate the dark current constants (converting E from eV to J by removing q)
+    % Calculate the dark current constants (converting b from nm to m)
     Jd = zeros(1,5);
-    Jd(1) = par.q * par.b * par.beta * par.ni2 * exp(par.Vbi/par.VT);
+    Jd(1) = par.q * par.b * par.beta * par.ni2 * exp(par.Vbi/par.VT) * 1e-9;
     Jd(2) = (par.q * par.b * par.dH * par.gv / (par.taup * par.gvH)) ...
-        * exp((par.Ev - par.EvH)/par.VT);
+        * exp((par.Ev - par.EvH)/(par.VT)) * 1e-9;
     Jd(3) = (par.q * par.b * par.dE * par.gc / (par.taun * par.gcE)) ...
-        * exp(-(par.Ec - par.EcE)/par.VT);
+        * exp(-(par.Ec - par.EcE)/(par.VT)) * 1e-9;
     Jd(4) = (par.q * par.vpE * par.dH * par.gv / par.gvH) ...
-        * exp((par.Ev - par.EvH)/par.VT);
+        * exp((par.Ev - par.EvH)/(par.VT));
     Jd(5) = (par.q * par.vnH * par.dE * par.gc / par.gcE) ...
-        * exp(-(par.Ec - par.EcE)/par.VT);
+        * exp(-(par.Ec - par.EcE)/(par.VT));
     
     % Calculate the potential barrier functions
     F = zeros(1,5);
@@ -52,6 +56,10 @@ function J = calculate_JPSC(par, Vin, options)
     Jd_sol = Jd(maxI);
     F_sol = F(maxI);
     
+    fprintf('R chosen: %f\n', maxI);
+    fprintf('Jd_sol: %f\n', Jd_sol);
+    fprintf('F_sol: %f\n', F_sol);
+    
     % Calculate the current density through the PSC current source
-    J = par.Js - Jd_sol * exp(-F_sol/par.VT);
+    J = par.Jsc - Jd_sol * exp(-F_sol/par.VT);
 end
