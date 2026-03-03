373b1f7ec1fd981f281dc4ddfab6cf2406a0d3c2
Isaac <shug7626@OX.AC.UK>
2026-03-03 19:57:11 +0000

    Added the permittivity of free space where relative permittivity was used

--- SinglePVK/SinglePVKmain.m
+++ SinglePVK/SinglePVKmain.m
@@ -29,10 +29,10 @@ a = par.validE * par.a;
 etac = par.validE * par.etac;
 
 % Evaluate the short circuit current (A cm-2)
-Jsc = par.q * sum(etac .* (ones(size(R)) - R) .* a .* (spectrums.bs .* par.validE));
+par.Jsc = par.q * sum(etac .* (ones(size(R)) - R) .* a .* (spectrums.bs .* par.validE));
 
 % Convert to mA
-Jsc = Jsc * 1e3;
+par.Jsc = par.Jsc * 1e3;
 
 
 
@@ -43,15 +43,37 @@ par.VT = par.k * par.T / par.q;
 % Built-in voltage (V) (converting the energies from eV to J)
 par.Vbi = (par.EcE - par.EvH) + par.VT*log((par.dH * par.dE)/(par.gvH * par.gcE));
 
+% nb and pb
+par.nb = (par.dE * par.gc / par.gcE) * exp((par.EcE - par.Ec)/par.VT);
+par.pb = (par.dH * par.gv / par.gvH) * exp((par.Ev - par.EvH)/par.VT);
+
 % Debye length (m)
-par.LD = sqrt((par.epsA * par.VT)/(par.q * par.N0));
-par.omegaE = sqrt((par.epsA * par.N0)/(par.epsE * par.dE));
-par.omegaH = sqrt((par.epsA * par.N0)/(par.epsH * par.dH));
+par.LD = sqrt((par.epsA * par.eps0 * par.VT)/(par.q * par.N0));
+par.omegaE = sqrt((par.epsA * par.eps0 * par.N0)/(par.epsE * par.eps0 * par.dE));
+par.omegaH = sqrt((par.epsA * par.eps0 * par.N0)/(par.epsH * par.eps0 * par.dH));
 
 % Intrinsic carrier concentration
 par.ni2 = par.gc * par.gv * exp(-(par.Ec - par.Ev)/(par.k * par.T));
 
 % Q(V) pre-multiplier
-par.Q0 = sqrt(2 * par.q * par.N0 * par.epsA * par.VT);
+par.Q0 = sqrt(2 * par.q * par.N0 * par.epsA * par.eps0 * par.VT);
+
+
+
+%% Calculate the open circuit voltage
+% Set the fsolve options
+options = optimoptions('fsolve', 'Display', 'none');
+
+V = linspace(0, 0.725, 20);
+J = zeros(size(V));
+
+for iter = 1:20
+    % Calculate J
+    J(iter) = calculate_JPSC(par, V(iter), options);
+end
+
+plot(V, J);
+
+
 
 
