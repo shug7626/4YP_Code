73b4260fb683eb2bfc5214ab9c483e0b65f9675c
Isaac <shug7626@OX.AC.UK>
2026-03-22 12:12:03 +0100

    Set out the variables and method

--- Optimisation/optimisation_main.m
+++ Optimisation/optimisation_main.m
@@ -1,2 +1,11 @@
 % Script to perform an optimisation of a PVK-on-Si Tandem cell
 
+% Parameters to be optimised/varied:
+% PVK Active Layer (PAL) thickness
+% Silicon total thickness
+
+% General Method:
+% Within the limits set, compute the Maximum Power Point (MPP) for a range
+% of values.
+% Starting with the maximum of those values, use a numerical method to find
+% the true MPP in the set range.
