4a15d72df65d64db479d0fab8a9320ac89962fb9
Isaac <shug7626@OX.AC.UK>
2026-03-22 12:33:18 +0100

    Imports the spectrum data and converts from cumulative wavelength to density of energies

--- Optimisation/optimisation_main.m
+++ Optimisation/optimisation_main.m
@@ -17,3 +17,20 @@ par = parameters();
 % Set the range of values to calculate the MPP for
 PVKRange = linspace(par.thick1Min, par.thick1Max, par.N);
 SiliconRange = linspace(par.thick2Min, par.thick2Max, par.N);
+
+
+
+%% Process the Spectrum
+% Import the spectrum data
+spectrum_table = readmatrix("Spectrum_full.xlsx");
+spectrums.wavelengths = spectrum_table(:,1);
+spectrums.bs_cumulative = spectrum_table(:,2);      % (photons cm-2 s-1)
+
+% Switch from the cumulative spectrum to the photon flux density
+cumulative_spec = 0;
+spectrums.bs = zeros(size(spectrums.bs_cumulative));
+spectrums.bs(1) = spectrums.bs_cumulative(1);
+spectrums.bs(2:end) = spectrums.bs_cumulative(2:end) - spectrums.bs_cumulative(1:end-1);
+
+% Convert the spectrums.wavelengths to photon energies (converting from nm to m)
+E = par.h * par.c ./(spectrums.wavelengths / 1e9);  % (eV)
