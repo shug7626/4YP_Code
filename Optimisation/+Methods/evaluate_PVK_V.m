87b9bd5e2dd0059b63e3820b623330343c786334
Isaac <shug7626@OX.AC.UK>
2026-03-22 14:12:58 +0100

    Copied the functions from the current scan method to directly calculate the voltage of the two sub cells

---
+++ Optimisation/+Methods/evaluate_PVK_V.m
@@ -0,0 +1,6 @@
+% Function to be used to calculate the voltage of the PSC at a given
+% voltage
+
+function F = evaluate_PVK_V(v1, J, par, options)
+    F = calculate_JPSC(par, v1, options) - (v1/(par.Rsh1 * par.A)) - J;
+end
