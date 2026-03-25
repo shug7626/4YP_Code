21c1826c61b5ff1b7144d2bafb3bbe0ed399ec8f
Isaac <shug7626@OX.AC.UK>
2026-03-25 21:32:27 +0100

    Change silicon units to be displayed in micrometers

--- Optimisation/+Plotting/Surface.m
+++ Optimisation/+Plotting/Surface.m
@@ -4,9 +4,15 @@
 function fig = Surface(PVKRange, SiliconRange, MPP)
     fig = figure(1);
 
+    % Change the silicon thickness units to be in micometers
+    SiliconRange = SiliconRange * 1e6;
+    
+    % Plot
     surf(PVKRange, SiliconRange, MPP);
+
+    % Label
     title('Plot of the Maximum Power Point (MPP) for the Range of Thicknesses');
     xlabel('PVK Thickness (nm)');
-    ylabel('Silicon Thickness (m)');
+    ylabel('Silicon Thickness ($\mu$m)', 'Interpreter', 'latex');
     zlabel('MPP (mW)');
 end
