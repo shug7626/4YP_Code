2ebfe05a156fb9c9d34310235461ede30f171f4a
Isaac <shug7626@OX.AC.UK>
2026-03-26 21:44:05 +0000

    Added the code to produce the surface plot

--- Optimisation/+Plotting/SurfaceV2.m
+++ Optimisation/+Plotting/SurfaceV2.m
@@ -1,1 +1,20 @@
 % Function to produce a surface plot of the MPPs from optimisationV2_main
+
+function fig = SurfaceV2(PVKRange, SiRange, MPP, par)
+    fig = figure(1);
+
+    % Change the silicon thickness units
+    SiRange = SiRange * 1e6;
+
+    % Plot using a For loop
+    for iter = 1:par.NTotal
+        surf(PVKRange(iter, :), SiRange(iter, :), MPP(iter, :, :));
+        hold on;
+    end
+
+    % Label
+    title('Maximum Power Point (MPP) for the Range of Thicknesses');
+    xlabel('PVK Thickness (nm)', 'Interpreter', 'latex');
+    ylabel('Silicon Thickness ($\mu$m)', 'Interpreter', 'latex');
+    zlabel('MPP (mW)', 'Interpreter', 'latex');
+end
