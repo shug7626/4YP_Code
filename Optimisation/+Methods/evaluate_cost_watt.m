7d4fb28e00fdaac1c31bb0ee5b721d49da6d54c1
Isaac <shug7626@OX.AC.UK>
2026-03-26 14:11:39 +0000

    Added the function to calculate the cost per watt at a given thickness

--- Optimisation/+Methods/evaluate_cost_watt.m
+++ Optimisation/+Methods/evaluate_cost_watt.m
@@ -1,1 +1,17 @@
 % Function to calculate the cost per watt for a set of thicknesses
+
+function cost_watt = evaluate_cost_watt(x, par, spectrums, options)
+    % Unpack input variables
+    PVKThick = x(1);
+    SiThick = x(2);
+
+    % Calculate the MPP
+    MPP = Methods.evaluate_MPP(x, par, spectrums, options);
+
+    % Calculate the cost
+    cost = par.baselineCost + ... 
+        ((par.PVKCost * PVKThick * 1e-6)' * (par.SiCost * SiThick * 1e3));
+    
+    % Return the cost/watt
+    cost_watt = (cost / MPP) * 1e3;
+end
