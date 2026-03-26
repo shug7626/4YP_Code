cdd37e2a6da0a22bbad20fbaf7263ae58b38c356
Isaac <shug7626@OX.AC.UK>
2026-03-26 13:12:31 +0000

    Added the code from the main script

--- Optimisation/+Methods/evaluate_MPP.m
+++ Optimisation/+Methods/evaluate_MPP.m
@@ -1,1 +1,19 @@
 % Function to find the MPP for a set of input thicknesses
+
+function MPP = evaluate_MPP(x, par, spectrums, options)
+    % Unpack the input variables
+    PVKThick = x(1);
+    SiThick = x(2);
+
+    % Calculate the short circuit current densities
+    [Jsc1, Jsc2] = Methods.calculate_Jsc(par, spectrums, PVKThick, SiThick);
+
+    % Create the power function
+    P = @(j) -1*j*Methods.evaluate_V(j, Jsc1, Jsc2, PVKThick, par, options);
+
+    % Use fminbnd to find the minimum negative power
+    [Jmpp_res, MPP_res] = fminbnd(P, 0, min([Jsc1 Jsc2]));
+
+    % Return result
+    MPP = -1 * MPP_res;
+end
