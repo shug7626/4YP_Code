72eab2f980a61dfc313cbb6c6b24450db950bf2f
Isaac <shug7626@OX.AC.UK>
2026-03-26 20:42:16 +0000

    Added the method to be used in comments at the top of the script

--- Optimisation/optimisationV2_main.m
+++ Optimisation/optimisationV2_main.m
@@ -1,2 +1,9 @@
 % Script to find the peak Maximum Power Point (MPP) and minimum cost per
 % watt using an alternative method to the original optimisation script
+
+% Method:
+% Calculate the MPP for a range of values within the bounds set in the
+% parameters file
+% Find the peak MPP from that calculation and perform another set of MPP
+% calculations around the previous peak
+% Repeat the 'zooming in' until the desired precision has been achieved
