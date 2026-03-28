00f08806ee1580efa01f360b817d021a65eadc75
Isaac <shug7626@OX.AC.UK>
2026-03-28 12:46:31 +0000

    Reformatted to make more readable

--- Optimisation/+Methods/evaluate_PVK_V.m
+++ Optimisation/+Methods/evaluate_PVK_V.m
@@ -2,5 +2,7 @@
 % voltage
 
 function F = evaluate_PVK_V(v1, J, Jsc1, thick1, par, options)
-    F = Methods.calculate_JPSC(par, v1, Jsc1, thick1, options) - (v1/(par.Rsh1 * par.A)) - J;
+    F = Methods.calculate_JPSC(par, v1, Jsc1, thick1, options) ...
+        - (v1/(par.Rsh1 * par.A)) ...
+        - J;
 end
