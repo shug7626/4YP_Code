fe6e95eb22a7dabba3227db230304c38daadf8e5
Isaac <shug7626@OX.AC.UK>
2026-03-29 18:14:18 +0100

    Corrected variable structure disagreements

--- DopingVarying/+Methods/solve_pvk_v.m
+++ DopingVarying/+Methods/solve_pvk_v.m
@@ -1,6 +1,6 @@
 % Function to be solved by fsolve to determine the four voltages in the PVK
 
-function F = solve_pvk_v(x, V, p)
+function F = solve_pvk_v(x, V, p, res)
     % Unpack inputs
     V1 = x(1);
     V2 = x(2);
@@ -9,17 +9,17 @@ function F = solve_pvk_v(x, V, p)
     Q = x(5);
     
     % V1 equation
-    F(1) = (-p.omegaE * Q) - Methods.evaluate_Q(V1, p);
+    F(1) = (-res.omegaE * Q) - Methods.evaluate_Q(V1, p, res);
     
     % V2 equation
-    F(2) = -Q - Methods.evaluate_Q(V2, p);
+    F(2) = -Q - Methods.evaluate_Q(V2, p, res);
     
     % V3 equation
-    F(3) = Q - Methods.evaluate_Q(V3, p);
+    F(3) = Q - Methods.evaluate_Q(V3, p, res);
     
     % V4 equation
-    F(4) = (-p.omegaH * Q) - Methods.evaluate_Q(V4, p);
+    F(4) = (-res.omegaH * Q) - Methods.evaluate_Q(V4, p, res);
     
     % Total voltage
-    F(5) = V1 + V2 + V3 + V4 - (p.Vbi1 - V);
+    F(5) = V1 + V2 + V3 + V4 - (res.Vbi1 - V);
 end
