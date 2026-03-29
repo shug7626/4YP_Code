4f8667b23105fd19f17482fd5aa91c95e9022e43
Isaac <shug7626@OX.AC.UK>
2026-03-29 17:57:25 +0100

    Moved the built in voltage calculation out of the function. Added the illumination current calculation. Corrected how the open circuit voltage function is called.

--- DopingVarying/+Methods/calculate_silicon_const.m
+++ DopingVarying/+Methods/calculate_silicon_const.m
@@ -4,9 +4,6 @@
 function res = calculate_silicon_const(Na, Nd, par, res)
     n2 = Nd;
     p2 = Na;
-
-    % Built in voltage
-    res.Vbi2 = par.VT * log(Na * Nd / (par.ni2 ^ 2));
     
     % Diffusion current constant
     res.Jdiff02 = par.q * (par.ni2^2) * ((par.Dn2 / (Na * par.Ln2)) + (par.Dp2 / (Nd * par.Lp2)));
@@ -20,8 +17,11 @@ function res = calculate_silicon_const(Na, Nd, par, res)
     res.Jscr02 = par.ni2 * sqrt(2 * par.q * par.eps2 * ((1/Na) + (1/Nd)) * res.Vbi2 / (par.tn2 * par.tp2));
     res.Jscrd2 = sqrt(2 * par.q * par.eps2 * (Nd + Na) * res.Vbi2 / (par.tn2 * par.tp2));
 
+    % Illumination current (mA m-2)
+    res.Jillum2 = res.Jsc2 + res.Jdiff02 + res.Jrad02 + res.Jscr02;
+
     % Calculate the silicon open circuit voltage
     % Find where the silicon current is zero
-    si_Voc_func = @(v) evaluate_si_Voc(v, par, res);
-    res.Voc2 = fzero(si_Voc_func, res.Vbi2/2);
+    si_Voc_func = @(v) Methods.evaluate_si_Voc(v, par, res);
+    res.Voc2 = fzero(si_Voc_func, res.Vbi2 / 2);
 end
