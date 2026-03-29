0c4a4d6d39912bc9b499618aba9725f06a514072
Isaac <shug7626@OX.AC.UK>
2026-03-29 23:25:07 +0100

    Added the function to plot the voltage area plot

---
+++ DopingVarying/+Plotting/V_Area.m
@@ -0,0 +1,38 @@
+% Area plot showing the components of the voltage
+
+function fig = V_Area(res, setting)
+    % Clear figure 2 if already open
+    clf(figure(2));
+
+    % Set the axes numbers font sizes
+    set(groot, 'defaultAxesFontSize', setting.axes_numbers);
+    
+    % Plot in figure 2
+    fig = figure(2);
+    Y = [(res.V1T.') (res.V2T.')];
+    x = res.V;
+    area(x, Y);
+
+    % Add a bold line to show the sum of the voltages
+    hold on;
+    V_total = res.V1T + res.V2T;
+    plot(x, V_total, 'k-', 'LineWidth', setting.line_width);
+
+    % Add lines to show the open circuit voltage of the entire cell
+    xline(res.Voc1 + res.Voc2, 'r');
+    yline(res.Voc1 + res.Voc2, 'r');
+
+    % Add a legend
+    legend({'V1T','V2T','Total Voltage'});
+
+    % Tile and labels
+    title('Voltage Contribuion from the Two Layers', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.title_font);
+    xlabel('Bias Voltage $\left(\mathrm{V}\right)$', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.axes_font);
+    ylabel('Voltage Components $\left(\mathrm{V}\right)$', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.axes_font);
+end
