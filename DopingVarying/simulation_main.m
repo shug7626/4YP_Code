63e1c2a4aa2d4e9b7f32d890cbfd819aa4dd0866
Isaac <shug7626@OX.AC.UK>
2026-03-29 16:09:17 +0100

    Added the calculations of the material constants

--- DopingVarying/simulation_main.m
+++ DopingVarying/simulation_main.m
@@ -1,1 +1,16 @@
 % Script to simulate a PVK-Si tandem cell
+
+%% Feth the Parameters
+par = parameters();
+
+
+
+%% Calculate the Constants
+% Calculate the thermal voltage
+par.VT = par.k*par.T/par.q;
+
+% Calculate the silicon constants
+par = Methods.calculate_silicon_const(par.Na2, par.Nd2, par);
+
+% Calculate the PVK constants
+par = Methods.calculate_pvk_const(par);
