1001b86bb225ea36ff91df1f5cfb0ff359314e24
Isaac <shug7626@OX.AC.UK>
2026-03-29 21:28:46 +0100

    Added variable font sizes depending on what's been set in the plot_settings file

--- DopingVarying/+Plotting/J_V.m
+++ DopingVarying/+Plotting/J_V.m
@@ -1,8 +1,11 @@
 % Current density - Voltage Plot
 
-function fig = J_V(res)
+function fig = J_V(res, setting)
     % Clear data from the figure if already open
     clf(figure(1));
+
+    % Set the axes numbers font sizes
+    set(groot, 'defaultAxesFontSize', setting.axes_numbers);
 
     % Plot in figure 1
     fig = figure(1);
@@ -10,30 +13,48 @@ function fig = J_V(res)
 
     % Plot the total cell J-V curve
     ax1 = nexttile;
-    plot(res.V, res.J);
+    plot(res.V, res.J, 'LineWidth', setting.line_width);
     xline(0);
     yline(0);
-    title('PVK-Si Tandem Cell Current Density - Voltage');
-    xlabel('Bias Voltage (V)', 'Interpreter', 'latex');
-    ylabel('Current Density (mA $\text{cm}^{-1}$)', 'Interpreter', 'latex');
+    title('PVK-Si Tandem Cell Current Density - Voltage', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.title_font);
+    xlabel('Bias Voltage (V)', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.axes_font);
+    ylabel('Current Density $\left(\mathrm{mA~cm}^{-1}\right)$', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.axes_font);
 
     % Plot cell 1's J-V curve
     ax2 = nexttile;
-    plot(res.V1T, res.J);
+    plot(res.V1T, res.J, 'LineWidth', setting.line_width);
     xline(0);
     yline(0);
-    title('Cell 1 J-V');
-    xlabel('Cell 1 Voltage (V)', 'Interpreter', 'latex');
-    ylabel('Cell 1 Current Density (mA $\text{cm}^{-1}$)', 'Interpreter', 'latex');
+    title('Cell 1 J-V', 'Interpreter', 'latex', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.title_font);
+    xlabel('Cell 1 Voltage (V)', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.axes_font);
+    ylabel('Cell 1 Current Density $\left(\mathrm{mA~cm}^{-1}\right)$', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.axes_font);
 
     % Plot cell 2's J-V curve
     ax3 = nexttile;
-    plot(res.V2T, res.J);
+    plot(res.V2T, res.J, 'LineWidth', setting.line_width);
     xline(0);
     yline(0);
-    title('Cell 2 J-V');
-    xlabel('Cell 2 Voltage (V)', 'Interpreter', 'latex');
-    ylabel('Cell 2 Current Density (mA $\text{cm}^{-1}$)', 'Interpreter', 'latex');
+    title('Cell 2 J-V', 'Interpreter', 'latex', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.title_font);
+    xlabel('Cell 2 Voltage (V)', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.axes_font);
+    ylabel('Cell 2 Current Density $\left(\mathrm{mA~cm}^{-1}\right)$', ...
+        'Interpreter', 'latex', ...
+        'FontSize', setting.axes_font);
 
     % Set the y-axis to the same for all three plots
     linkaxes([ax1 ax2 ax3], 'y');
