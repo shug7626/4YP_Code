b8e91e8378efca6a741ef4654bc48130575c66f6
Isaac <shug7626@OX.AC.UK>
2026-03-29 15:34:51 +0100

    Added the code to calculate the PVK constants

--- DopingVarying/+Methods/calculate_pvk_const.m
+++ DopingVarying/+Methods/calculate_pvk_const.m
@@ -1,1 +1,21 @@
 % Script to calculate the PVK constants
+
+function par = calculate_pvk_const(par)
+    % Built-in voltage (V) (converting the energies from eV to J)
+    par.Vbi1 = (par.EcE - par.EvH) + par.VT*log((par.dH * par.dE)/(par.gvH * par.gcE));
+    
+    % nb and pb
+    par.nb = (par.dE * par.gc / par.gcE) * exp((par.EcE - par.Ec)/par.VT);
+    par.pb = (par.dH * par.gv / par.gvH) * exp((par.Ev - par.EvH)/par.VT);
+    
+    % Debye length (m)
+    par.LD = sqrt((par.epsA * par.eps0 * par.VT)/(par.q * par.N0));
+    par.omegaE = sqrt((par.epsA * par.eps0 * par.N0)/(par.epsE * par.eps0 * par.dE));
+    par.omegaH = sqrt((par.epsA * par.eps0 * par.N0)/(par.epsH * par.eps0 * par.dH));
+    
+    % Intrinsic carrier concentration
+    par.ni2 = par.gc * par.gv * exp(-(par.Ec - par.Ev)/(par.k * par.T));
+    
+    % Q(V) pre-multiplier
+    par.Q0 = sqrt(2 * par.q * par.N0 * par.epsA * par.eps0 * par.VT);
+end
