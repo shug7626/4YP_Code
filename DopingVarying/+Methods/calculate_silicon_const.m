1765dad45dfc7f0ec76b9c7cfd352fe760f3bfd7
Isaac <shug7626@OX.AC.UK>
2026-03-29 16:08:17 +0100

    Now set the electron and hole concentrations at the start of the function

--- DopingVarying/+Methods/calculate_silicon_const.m
+++ DopingVarying/+Methods/calculate_silicon_const.m
@@ -2,6 +2,9 @@
 % concentrations
 
 function par = calculate_silicon_const(Na, Nd, par)
+    n2 = Nd;
+    p2 = Na;
+
     % Built in voltage
     par.Vbi2 = par.VT * log(Na * Nd / (par.ni2 ^ 2));
     
@@ -10,13 +13,13 @@ function par = calculate_silicon_const(Na, Nd, par)
     par.Jdiffd2 = par.q * Nd * Na * ((par.Dn2 / (Na * par.Ln2)) + (par.Dp2 / (Nd * par.Lp2)));
     
     % Radiative recombination current constant
-    par.Jrad02 = par.beta2 * ((par.n2 * par.p2) - (par.ni2 ^ 2));
-    par.Jradd2 = par.beta2 * ((par.n2 * par.p2 * exp(par.Vbi2/par.VT)) - (Nd * Na));
+    par.Jrad02 = par.beta2 * ((n2 * p2) - (par.ni2 ^ 2));
+    par.Jradd2 = par.beta2 * ((n2 * p2 * exp(par.Vbi2/par.VT)) - (Nd * Na));
     
     % Recombination in the depletion region (SCR) current constant
     par.Jscr02 = par.ni2 * sqrt(2 * par.q * par.eps2 * ((1/Na) + (1/Nd)) * par.Vbi2 / (par.tn2 * par.tp2));
     par.Jscrd2 = sqrt(2 * par.q * par.eps2 * (Nd + Na) * par.Vbi2 / (par.tn2 * par.tp2));
 
     % Depletion region width
-    par.W2 = sqrt(2 * par.eps2 * par.Vbi2 * ((1/Na) + (1/Nd)) / par.q);
+    par.W2 = sqrt(2 * par.eps2 * par.Vbi2 * ((1/Na) + (1/Nd)) / par.q);         % (cm)
 end
