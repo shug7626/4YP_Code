dc6b4905ef38701023d8c8f04f4e55bea363f638
Isaac <shug7626@OX.AC.UK>
2026-03-29 18:14:45 +0100

    Corrected variable structure disagreements

--- DopingVarying/+Methods/evaluate_Q.m
+++ DopingVarying/+Methods/evaluate_Q.m
@@ -1,5 +1,5 @@
 % Script to store the fuction which returns the value of Q(nu)
 
-function Q = evaluate_Q(nu, p)
-    Q = p.Q0 * sign(nu) * sqrt(exp(nu/p.VT) - (nu/p.VT) - 1);
+function Q = evaluate_Q(nu, p, res)
+    Q = res.Q0 * sign(nu) * sqrt(exp(nu/p.VT) - (nu/p.VT) - 1);
 end
