473084c487dc6f0d036314d639c04ccac33ad5e8
Isaac <shug7626@OX.AC.UK>
2026-03-30 20:01:09 +0100

    Added the loop decoding

--- DopingVarying/optimisation_main.m
+++ DopingVarying/optimisation_main.m
@@ -12,6 +12,9 @@
 par = parameters();
 set = plot_settings();
 
+% Set the number of MPPs to calculate
+N = par.N2;
+
 % Set the numerical solver settings
 options = optimoptions('fsolve', ...
     'Display', 'none', ...
@@ -32,11 +35,36 @@ par.VT = par.k*par.T/par.q;
 
 %% Create the matrices from the boundaries set in the parameters file
 % PVK PAL thicknesses
-pvk_thicks = linspace(par.thick1Min, par.thick1Max, par.N2);
+pvk_thicks = linspace(par.thick1Min, par.thick1Max, N);
 
 % Si doping concentrations
-Nds = linspace(par.NdMin, par.NdMax, par.N2);
-Nas = linspace(par.NaMin, par.NaMax, par.N2);
+Nds = linspace(par.NdMin, par.NdMax, N);
+Nas = linspace(par.NaMin, par.NaMax, N);
 
 % Create the MPP matrix to store the results
-MPPs = zeros(par.N2, par.N2, par.N2);
+MPPs = zeros(par.N2, par.N2, N);
+
+% 1-dimension version of MPPs
+MPP_temp = zeros(1, N^3);
+
+
+
+%% Calculate the MPP over the Ranges
+fprintf('Starting the calculation of the MPPs\n');
+
+% Temporary for checking dimensions
+pvk_thicks = linspace(0, N^3 - N^2, N);
+Nds = linspace(0, N^2 - N, N);
+Nas = linspace(1, N, N);
+
+% Use a parfor loop to loop through each option
+for iter = 1:N^3
+    pvk_thick = pvk_thicks(floor((iter - 1)/(N ^ 2)) + 1);
+    % Nd = Nds(mod(floor((iter)/N), N) + 1);
+    Nd = Nds(floor((mod(iter - 1, N^2))/N) + 1);
+    Na = Nas(mod((iter - 1), N) + 1);
+
+    MPP_temp(iter) = sum([pvk_thick Nd Na]);
+end
+
+fprintf('MPP Calculation Complete\n');
