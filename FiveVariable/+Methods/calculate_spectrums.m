d83acd06e503baa62537c07c08255ba7e0b635d2
Isaac <shug7626@OX.AC.UK>
2026-03-31 19:09:43 +0100

    Now calculates the refractive index and absorptivity coefficients for the wavelengths of the incident spectrum

--- FiveVariable/+Methods/calculate_spectrums.m
+++ FiveVariable/+Methods/calculate_spectrums.m
@@ -1,7 +1,7 @@
 % Function to read the spectrum data and return a spectrums structure
 
 function s = calculate_spectrums(par)
-    % Import spectrum data
+    %% Import spectrum data
     opts = detectImportOptions('+Data/Spectrum_full.xlsx', 'Sheet', 2);
     opts.SelectedVariableNames = opts.VariableNames([1, 3]);
     s.full = readmatrix('+Data/Spectrum_full.xlsx', opts);
@@ -32,4 +32,64 @@ function s = calculate_spectrums(par)
         / (s.E(last1) - s.E(last1 + 1));
     s.validE2(last2 + 1) = (s.E(last2) - (par.Ec - par.Ev)) ...
         / (s.E(last2) - s.E(last2 + 1));
+
+    %% Import the wavelength dependent data
+    % PVK data
+    opts = detectImportOptions('+Data/Perovskite_data.csv');
+    opts.DataLines = [2, Inf];
+    opts.SelectedVariableNames = opts.VariableNames([1, 2, 4, 5]);
+    PVK = readmatrix('+Data/Perovskite_data.csv', opts);
+
+    % Si data
+    opts = detectImportOptions('+Data/Si_Crystalline_data.csv');
+    opts.DataLines = [2, Inf];
+    opts.SelectedVariableNames = opts.VariableNames([1, 2, 4, 5]);
+    Si = readmatrix('+Data/Si_Crystalline_data.csv', opts);
+
+    % Use a for loop and interpolation to find the refractive index and
+    % absorption coefficient for each wavelength/energy
+    for iter = 1:length(s.wavelengths)
+        lambda = s.wavelengths(iter);
+        if lambda < PVK(1, 1)
+            s.PVK(iter, 1) = PVK(1, 2);
+            s.PVK(iter, 2) = PVK(1, 4);
+        elseif lambda > PVK(end, 1)
+            s.PVK(iter, 1) = PVK(end, 2);
+            s.PVK(iter, 2) = PVK(end, 4);
+        else
+            index = find(PVK(:, 1) <= lambda, 1, 'last');
+            if index >= length(PVK(:, 1))
+                index2 = length(PVK(:,1));
+            else
+                index2 = index + 1;
+            end
+            frac = (lambda - PVK(index, 1)) ...
+                / (PVK(index2, 1) - PVK(index, 1));
+            s.PVK(iter, 1) = PVK(index, 2) + ...
+                frac * (PVK(index2, 2) - PVK(index, 2));
+            s.PVK(iter, 2) = PVK(index, 4) + ...
+                frac * (PVK(index2, 4) - PVK(index, 4));
+        end
+
+        if lambda < Si(1, 1)
+            s.Si(iter, 1) = Si(1, 2);
+            s.Si(iter, 2) = Si(1, 4);
+        elseif lambda > Si(end, 1)
+            s.Si(iter, 1) = Si(end, 2);
+            s.Si(iter, 2) = Si(end, 4);
+        else
+            index = find(Si(:, 1) <= lambda, 1, 'last');
+            if index >= length(Si(:, 1))
+                index2 = length(Si(:,1));
+            else
+                index2 = index + 1;
+            end
+            frac = (lambda - Si(index, 1)) ...
+                / (Si(index2, 1) - Si(index, 1));
+            s.Si(iter, 1) = Si(index, 2) + ...
+                frac * (Si(index2, 2) - Si(index, 2));
+            s.Si(iter, 2) = Si(index, 4) + ...
+                frac * (Si(index2, 4) - Si(index, 4));
+        end
+    end
 end
