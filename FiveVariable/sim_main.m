dd61823bd6bc569c078695fcbf0ca59ea8a09824
Isaac <shug7626@OX.AC.UK>
2026-03-31 16:13:24 +0100

    Added the constants calculation for calculating the voltages

--- FiveVariable/sim_main.m
+++ FiveVariable/sim_main.m
@@ -8,7 +8,29 @@ par = Data.parameters();
 % Import spectrum data
 spectrums = Methods.calculate_spectrums(par);
 
+% Set the numerical solver settings
+options = optimoptions('fsolve', ...
+    'Display', 'none', ...
+    'FunctionTolerance', 1e-16, ...
+    'OptimalityTolerance', 1e-16, ...
+    'StepTolerance', 1e-16);
+
 
 
 time.init = toc;
 fprintf('Import complete in %f seconds\n', time.init);
+%% Calculate Constants
+tic;
+% Calculate the thermal voltage
+par.VT = par.k*par.T/par.q;
+
+% Create a variables variables as input
+vars = [par.thick1, par.thick2n, par.thick2p, par.Nd2, par.Na2];
+
+% Calculate the constants to be used for the voltage calculations
+res = Methods.calculate_const(vars, par, spectrums);
+
+
+
+time.const = toc;
+fprintf('Constants calculated in %f seconds\n', time.const);
