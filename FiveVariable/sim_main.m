873822b74ae6733f1f777581452486945f8418e2
Isaac <shug7626@OX.AC.UK>
2026-03-31 13:55:23 +0100

    Added the import section

--- FiveVariable/sim_main.m
+++ FiveVariable/sim_main.m
@@ -1,1 +1,14 @@
 % Script to simulate a single PVK-Si tandem solar cell
+
+%% Import Data
+tic;
+% Material parameters
+par = Data.parameters();
+
+% Import spectrum data
+spectrums = Methods.calculate_spectrums(par);
+
+
+
+time.init = toc;
+fprintf('Import complete in %f seconds\n', time.init);
