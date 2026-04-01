81bed49f95f7da1bdec7efd64e4b600c332d38c5
Isaac <shug7626@OX.AC.UK>
2026-04-01 02:08:51 +0100

    Function to calculate and return the short circuit current generated by the silicon cell

---
+++ FiveVariable/+Methods/calculate_si_Jsc.m
@@ -0,0 +1,34 @@
+% Function to return the photo generation current for the silicon cell
+
+function Jsc = calculate_si_Jsc(bs2, par, res, spectrums)
+    % Calculate the widths
+    Wn = par.thick2n - res.wn;
+    Wp = par.thick2p - res.wp;
+
+    % Calculate the array of integrations for the n-type
+    int_res_n = arrayfun(@(c) integral(@(x) ...
+        Methods.calculate_np_int([x, Wn - x], par.Lp2, par.Srear, par.Dp2, Wn, c), ...
+        0, Wn, spectrums.wavelengths));
+
+    % Calculate the array of integrations for the p-type
+    int_res_p = arrayfun(@(c) integral(@(x) ...
+        Methods.calculate_np_int([x, x - Wn - res.wn - res.wp], par.Ln2, par.Sfront, par.Dn2, Wp, c), ...
+        Wn + res.wn + res.wp, Wn + res.wn + res.wp + Wp, spectrums.wavelengths));
+
+    % Calculate the array of integrations for the depletion region
+    int_res_d = arrayfun(@(c) integral(@(x) ...
+        Methods.calculate_d_int(x, c), ...
+        Wn, Wn + res.wn + res.wp, spetrums.wavelengths));
+
+    % Calculate the current due to the bulk n-type
+    Jn = par.q * sum(bs2 .* spectrums.Si(:,1) .* int_res_n);
+
+    % Calculate the current due to the bulk p-type
+    Jp = par.q * sum(bs2 .* spectrums.Si(:,1) .* int_res_p);
+
+    % Calculate the current due to the depletion region
+    Jd = par.q * sum(bs2 .* spectrums.Si(:,1) .* int_res_d);
+
+    % Return the total current density
+    Jsc = Jn + Jd + Jp;
+end
