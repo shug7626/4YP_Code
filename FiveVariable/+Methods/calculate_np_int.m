f2e4638d271cd336322553b99b6d6156597bc8d0
Isaac <shug7626@OX.AC.UK>
2026-04-03 18:54:23 +0100

    Added a multiplier to the input variables to correct for units

--- FiveVariable/+Methods/calculate_np_int.m
+++ FiveVariable/+Methods/calculate_np_int.m
@@ -3,13 +3,13 @@
 
 function ret = calculate_np_int(x, L, S, D, W, a)
     % Unpack the input
-    x1 = x(1);
-    x2 = x(2);
+    x1 = x(1) * 1e-2;
+    x2 = x(2) * 1e-2;
 
     % Calculate the integral function
     ret = (cosh(x2 / L) - ...
         (((((S * L / D) * cosh(W / L)) + sinh(W / L)) ...
         / (((S * L / D) * sinh(W / L)) + cosh(W / L))) ...
         * sinh(x2 / L))) ...
-        * exp(-1 * a * 1e2 * x1);
+        * exp(-1 * a * x1);
 end
