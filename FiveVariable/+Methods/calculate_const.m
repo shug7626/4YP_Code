651ea2ee4fbadf5821eff7be60790a8ce06806dd
Isaac <shug7626@OX.AC.UK>
2026-04-03 19:46:48 +0100

    Removed unnecessary variable assignment and added comments for unit clarity

--- FiveVariable/+Methods/calculate_const.m
+++ FiveVariable/+Methods/calculate_const.m
@@ -3,9 +3,6 @@
 
 function res = calculate_const(x, par, spectrums)
     % Unpack the input variables
-    pvk_thick = x(1);
-    si_n_thick = x(2);
-    si_p_thick = x(3);
     Nd = x(4);
     Na = x(5);
 
@@ -13,8 +10,8 @@ function res = calculate_const(x, par, spectrums)
     res.Vbi2 = par.VT * log(Na * Nd / (par.ni2 ^ 2));
 
     % Calculate the silicon depletion region widths
-    w_pre = sqrt((2 * par.eps2 * res.Vbi2)/(par.q * ((1/Na) + (1/Nd))));
-    res.wn = (1/Nd) * w_pre;
+    w_pre = sqrt((2 * par.eps2 * res.Vbi2)/(par.q * ((1/Na) + (1/Nd))));    % (cm-2)
+    res.wn = (1/Nd) * w_pre;            % (cm)
     res.wp = (1/Na) * w_pre;
 
     % Calculate the short circuit current densities
