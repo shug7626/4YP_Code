397770f7a6e407af5172dc10d23487eac08bab5c
Isaac <shug7626@OX.AC.UK>
2026-04-03 23:50:38 +0100

    Returns the results of the integration

--- FiveVariable/+Methods/calculate_si_Jsc.m
+++ FiveVariable/+Methods/calculate_si_Jsc.m
@@ -7,24 +7,24 @@ function [Jsc, res] = calculate_si_Jsc(bs2, par, res, spectrums)
 
     % Generate the array of integrations for the p-type (front)
     p_func = @(a) integral(@(x) Methods.calculate_front_int(x, par, Wp, a), 0, Wp, 'ArrayValued', true);
-    int_res_p = -1 * arrayfun(p_func, spectrums.Si(:, 2));
+    res.int_res_p = -1 * arrayfun(p_func, spectrums.Si(:, 2));
 
     % Calculate the array of integrations for the n-type (rear)
     n_func = @(a) integral(@(x) Methods.calculate_rear_int(x, Wn, Wp, par, res, a), 0, Wn, 'ArrayValued', true);
-    int_res_n = -1 * arrayfun(n_func, spectrums.Si(:, 2));
+    res.int_res_n = -1 * arrayfun(n_func, spectrums.Si(:, 2));
 
     % Calculate the array of integrations for the depletion region
-    d_func = @(a) integral(@(x) Methods.calculate_d_int(x, a), Wp, (Wp + (res.wp + res.wn)*1e2), 'ArrayValued', true);
-    int_res_d = arrayfun(d_func, spectrums.Si(:, 2));
+    d_func = @(a) integral(@(x) Methods.calculate_d_int(x, a), Wp, Wp + ((res.wp + res.wn)*1e2), 'ArrayValued', true);
+    res.int_res_d = arrayfun(d_func, spectrums.Si(:, 2));
 
     % Calculate the current due to the bulk p-type
-    Jp = par.q * sum(bs2 .* spectrums.Si(:,2) .* int_res_p);
+    Jp = par.q * sum(bs2 .* spectrums.Si(:,2) .* res.int_res_p);
 
     % Calculate the current due to the bulk n-type
-    Jn = par.q * sum(bs2 .* spectrums.Si(:,2) .* int_res_n);
+    Jn = par.q * sum(bs2 .* spectrums.Si(:,2) .* res.int_res_n);
 
     % Calculate the current due to the depletion region
-    Jd = par.q * sum(bs2 .* spectrums.Si(:,2) .* int_res_d);
+    Jd = par.q * sum(bs2 .* spectrums.Si(:,2) .* res.int_res_d);
 
     % % Return the total current density
     Jsc = Jp + Jn + Jd;
