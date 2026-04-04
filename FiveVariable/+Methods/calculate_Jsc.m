9661d0c01f0e8d6c57086fe624181fd0ca7ebd1a
Isaac <shug7626@OX.AC.UK>
2026-04-04 01:12:46 +0100

    Now returns more data

--- FiveVariable/+Methods/calculate_Jsc.m
+++ FiveVariable/+Methods/calculate_Jsc.m
@@ -4,17 +4,17 @@ function res = calculate_Jsc(x, par, res, spectrums)
     % Unpack the input
     pvk_thick = x(1);
 
-    % % Create vectors for the reflectivity, absorptivity, and probability
-    R1 = spectrums.PVK(:, 3);
-    R2 = spectrums.Si(:, 3);
-    a1 = 1 - exp(-spectrums.PVK(:,2) * pvk_thick * 1e-7);
+    % Create vectors for the reflectivity, absorptivity, and probability
+    res.R1 = spectrums.PVK(:, 3);
+    res.R2 = spectrums.Si(:, 3);
+    res.a1 = 1 - exp(-spectrums.PVK(:,2) * pvk_thick * 1e-7);
     
     % Calculate the spectrum seen by cell 2
-    bs2 = (1-R1) .* (1-R2) .* (1-a1) .* spectrums.bs;
+    res.bs2 = (1-res.R1) .* (1-res.R2) .* (1-res.a1) .* spectrums.bs;
     
     % Evaluate Jsc1 and Jsc2
-    Jsc1 = par.q * sum((1-R1) .* a1 .* spectrums.bs .* spectrums.validE1);  % (A cm-2)
-    [Jsc2, res] = Methods.calculate_si_Jsc(bs2, par, res, spectrums);
+    Jsc1 = par.q * sum((1-res.R1) .* res.a1 .* spectrums.bs .* spectrums.validE1);  % (A cm-2)
+    [Jsc2, res] = Methods.calculate_si_Jsc(res.bs2, par, res, spectrums);
     
     % Convert to (mA cm-2)
     res.Jsc1 = Jsc1 * 1e3;
