b243d0d9cf5340a77446852660f06f6431b5aba4
Isaac <shug7626@OX.AC.UK>
2026-04-04 01:10:28 +0100

    Added the plots to the end of the script

--- FiveVariable/sim_main.m
+++ FiveVariable/sim_main.m
@@ -45,3 +45,124 @@ v_s = zeros(size(J));
 
 
 time.allocate = toc;
+%% Calculate V, V1, V2, Vs for a given J
+tic;
+fprintf('Starting the Calculation of the Voltages\n');
+% Calculate constants to be used in the initial guesses
+n_estimate = 1.1;
+jd01 = res.Jsc1 / exp((res.Voc1 - res.Vbi1) / (n_estimate * par.VT));
+jd02 = res.Jsc2 / exp((res.Voc2 - res.Vbi2) / (n_estimate * par.VT));
+
+% Loop throught J to calculate the voltages
+parfor iter = 2:par.N
+    % Set the initial guesses
+    v01 = res.Vbi1 + (n_estimate * par.VT * log((res.Jsc1 - J(iter))/jd01));
+    v02 = res.Vbi2 + (n_estimate * par.VT * log((res.Jsc2 - J(iter))/jd02));
+
+    % Initialise the functions to be used
+    pvk_V_func = @(v1) Methods.evaluate_PVK_V(v1, J(iter), par.thick1, par, res, options);
+    si_V_func = @(v2) Methods.evaluate_Si_V(v2, J(iter), par, res);
+
+    % Use fzero to solve
+    V1(iter) = fzero(pvk_V_func, v01);
+    V2(iter) = fzero(si_V_func, v02);
+
+    % Calculate the total cell voltage
+    v_s(iter) = -J(iter) * (par.Rs1 + par.Rs2) * par.A;
+    V(iter) = V1(iter) + V2(iter) + v_s(iter);
+end
+
+
+
+%% Add the Voltages at V=0, J=Jsc
+if res.Jsc1 == min([res.Jsc1 res.Jsc2])
+    % Set the initial guess
+    v02 = V2(2);
+
+    % Initialise the function
+    si_V_func = @(v2) Methods.evaluate_Si_V(v2, res.Jsc1, par, res);
+
+    % Use fzero to solve
+    V2(1) = fzero(si_V_func, v02);
+else
+    % Set the initial guess
+    v01 = V1(2);
+
+    % Initialise the function
+    pvk_V_func = @(v1) Methods.evaluate_PVK_V(v1, res.Jsc2, par.thick1, par, res, options);
+
+    % Use fzero to solve
+    V1(1) = fzero(pvk_V_func, v01);
+end
+
+% Include the series resistances voltage
+v_s(1) = -J(1) * (par.Rs1 + par.Rs2) * par.A;
+
+% Calculate the total voltage at Jsc
+V(1) = V1(1) + V2(1) + v_s(1);
+
+% Unpack the results
+res.V = V;
+res.J = J;
+res.V1 = V1;
+res.V2 = V2;
+res.v_s = v_s;
+
+
+time.voltages = toc;
+fprintf('Voltages calculated in %f seconds\n', time.voltages);
+%% Calculate Contribution of Each Cell to the Series Resistor Voltage
+tic;
+% Cell 1 series resistor voltage
+res.Vs1 = -res.J * par.A * par.Rs1;
+
+% Cell 2 series resistor voltage
+res.Vs2 = -res.J * par.A * par.Rs2;
+
+% Total cell contributions
+res.V1T = res.V1 + res.Vs1;
+res.V2T = res.V2 + res.Vs2;
+
+
+
+%% Calculate the Efficiency
+% Incident photon power (converting to (mW cm-2) from (W cm-2))
+Pin = sum(spectrums.bs .* spectrums.E) * par.q * 1e3;
+Pout = max(res.J .* res.V);
+Efficiency = Pout / Pin * 100;
+
+% Print the efficiency
+fprintf('Efficiency: %5.3f%%\n', Efficiency);
+
+
+
+time.aditional = toc;
+fprintf('Total time = %f seconds\n', sum(structfun(@(x) sum(x(:)), time)));
+%% Plots
+% Fetch the settings
+set = Data.plot_settings();
+
+% Plot the J_V plots
+if set.plot_j_v
+    fig1 = Plotting.J_V(res, set);
+end
+
+% Plot the voltage area plot
+if set.plot_V_area
+    fig2 = Plotting.V_Area(res, set);
+end
+
+% Plot the voltage - volage plots
+if set.plot_v_v
+    fig3 = Plotting.V_VBias(res, set);
+end
+
+% Plot the P-V plot
+if set.plot_p_v
+    fig4 = Plotting.P_V(res, set);
+end
+
+% Plot the spectrum data
+if set.plot_spectrum
+    fig5 = Plotting.spectrum(spectrums, set, par, res);
+end
