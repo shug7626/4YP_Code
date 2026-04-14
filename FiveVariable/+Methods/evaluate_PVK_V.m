206eae6ea216388d91170878e9f5c571275e3646
Isaac <shug7626@OX.AC.UK>
2026-04-14 21:54:13 +0100

    Updated the calculation of the PSC current density to take the new function outputs

--- FiveVariable/+Methods/evaluate_PVK_V.m
+++ FiveVariable/+Methods/evaluate_PVK_V.m
@@ -2,7 +2,8 @@
 % voltage
 
 function F = evaluate_PVK_V(v1, J, thick1, par, res, options)
-    F = Methods.calculate_JPSC(par, res, v1, thick1, options) ...
+    [J_PSC,V] = Methods.calculate_JPSC(par, res, v1, thick1, options);
+    F = J_PSC ...
         - (v1/(par.Rsh1 * par.A)) ...
         - J;
 end
